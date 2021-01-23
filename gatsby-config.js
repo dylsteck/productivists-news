2db0017d6e24bfd91fad8ab4131ce6e3eddb3102
Dylan Steck <12853808+dylsteck@users.noreply.github.com>
2021-01-23 18:59:23 +0000

    Added airtable plugin to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,25 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-airtable`,
+      options: {
+        apiKey: `keyq3oZ29RfC0jHoJ`, // may instead specify via env, see below
+        concurrency: 5, // default, see using markdown and attachments for more information
+        tables: [
+          {
+            baseId: `tblY0JXVDECYE46Iz`,
+            tableName: `PKM`,
+            separateNodeType: false, // boolean, default is false, see the documentation on naming conflicts for more information
+            separateMapType: false // boolean, default is false, see the documentation on using markdown and attachments for more information
+          },
+            baseId: `tblY0JXVDECYE46Iz`,
+            tableName: `PKM`
+            // can leave off queryName, mapping or tableLinks if not needed
+          }
+        ]
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
